fbf4614d309343c7fb2b8e3941f65029cdd4f86a
Arul Murugavel <63643748+BLooDBRothER@users.noreply.github.com>
2024-07-25 19:48:28 +0530

    Add routing support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:chat_app_firebase/api/firebase_message_api.dart';
+import 'package:chat_app_firebase/api/notification_api.dart';
 import 'package:chat_app_firebase/firebase_options.dart';
 import 'package:chat_app_firebase/screens/authentication.dart';
 import 'package:chat_app_firebase/screens/chat.dart';
+import 'package:chat_app_firebase/screens/settings/settings_home.dart';
 import 'package:chat_app_firebase/screens/splash_screen.dart';
 import 'package:chat_app_firebase/theme/theme_config.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,11 +12,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await NotificationApi.init();
+  await FirebaseMessageApi().initNotification();
 
   runApp(
     const ProviderScope(
@@ -25,25 +31,37 @@ void main() async {
 class MyChatApp extends StatelessWidget {
   const MyChatApp({super.key});
 
+  StreamBuilder _buildScreen(String route) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting) {
+          return const SplashScreen();
+        }
+        if(snapshot.hasData) {
+          switch(route) {
+            case "/":
+              return const ChatScreen();
+            case "/settings":
+              return const SettingScreen();
+          }
+        }
+        return const AuthenticationScreen();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FlutterChat',
       darkTheme: MyAppTheme.darkTheme,
       themeMode: ThemeMode.dark,
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if(snapshot.connectionState == ConnectionState.waiting) {
-            return const SplashScreen();
-          }
-          if(snapshot.hasData) {
-            return const ChatScreen();
-          }
-
-          return const AuthenticationScreen();
-        },
-      )
+      routes: {
+        "/": (context) => _buildScreen("/"),
+        "/settings": (context) => _buildScreen("/settings")
+      },
+      initialRoute: "/",
     );
   }
 }
