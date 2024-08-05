074c8ff6307401a3ef7b1a619f33c4715047206b
Arul Murugavel <63643748+BLooDBRothER@users.noreply.github.com>
2024-08-05 10:53:12 +0530

    Create base api configs

---
+++ lib/api/api_base.dart
@@ -0,0 +1,6 @@
+import 'package:chat_app_firebase/env.dart';
+
+const BASE_URL = "$API_DOMAIN/api";
+
+const NOTIFICATION_BASE = "$BASE_URL/notification";
+const CREATE_GROUP_NOTIFICATION = "$NOTIFICATION_BASE/create";
