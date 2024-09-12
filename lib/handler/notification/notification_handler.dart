8528ebb1ad12e1ad91977e168bc307b0ad51682f
Arul Murugavel <63643748+BLooDBRothER@users.noreply.github.com>
2024-09-12 19:05:56 +0530

    Remove unused imports

--- lib/handler/notification/notification_handler.dart
+++ lib/handler/notification/notification_handler.dart
@@ -1,5 +1,3 @@
-import 'package:chat_app_firebase/models/notification_payload_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 abstract class NotificationHandler<T> {
   void triggerNotificationOnClick({T? payload});
